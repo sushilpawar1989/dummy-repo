ae042d53a6bb97b0e817f27032d300db3f93c7c2
sushilpawar1989 <34710269+sushilpawar1989@users.noreply.github.com>
2017-12-20 17:37:16 +0530

    Update on-success.sh

--- on-success.sh
+++ on-success.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+# Automatically cherrypick the last commit through the following branches:
+# 2.1 -} 2.2 -} 2.3 -} master
 
-echo "present work dir $PWD"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LAST_COMMIT=$(git rev-list -1 HEAD)
 
-echo "python automerge.py $TRAVIS_BRANCH"
+echo last commit $LAST_COMMIT
+echo current branch $CURRENT_BRANCH
+
+echo ------------------------
+
+echo Automatic cherrypick last commit $LAST_COMMIT changes into 2.2,master
+
+git checkout 2.2
+git cherry-pick $LAST_COMMIT
+
+git checkout master
+git cherry-pick $LAST_COMMIT
